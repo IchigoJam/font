123d0e45197cbb652d0d7512c610843169333edf
Taisuke Fukuno <fukuno@jig.jp>
2024-01-01 08:41:29 +0900

    Update makeIchigoJamFont_round.js

--- makeIchigoJamFont_round.js
+++ makeIchigoJamFont_round.js
@@ -4,7 +4,7 @@ import { Base16 } from "https://code4fukui.github.io/Base16/Base16.js";
 import { Base2 } from "https://code4fukui.github.io/Base2/Base2.js";
 import { svg2path } from "./svg2path.js";
 
-const fontdata2dot = (font) => {
+export const fontdata2dot = (font) => {
   const res = [];
   for (let i = 7; i >= 0; i--) {
     res.push(Base2.encode(Base16.decode(font.substring(i * 2, i * 2 + 2))));
